b33c7873f6cdc63d17f8c67f8b5d3a2d619ac8d0
Nicolas-zv <nicolaszalazarUni2003@gmail.com>
2025-03-24 11:56:15 -0400

    Add files via upload

---
+++ tarea/tarea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+
+namespace OpenTK_U
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new OpenTK.Mathematics.Vector2i(800, 600),
+                Title = "Letter U 3D",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
